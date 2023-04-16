be5a3a0cc41208d5963eedd06fa015b7c74f97d9
Lee Youjin <kynhun20@gachon.ac.kr>
2023-04-16 10:40:11 +0900

    Baekjoon 27866 update

---
+++ ProblemSolving_27866/ProblemSolving_27866/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  ProblemSolving_27866
+//
+//  Created by YOUJIM on 2023/04/16.
+//
+
+import Foundation
+
+let s = readLine()!
+let i = Int(readLine()!)!
+let start = s.startIndex
+print(s[s.index(start, offsetBy: i - 1)])
+
