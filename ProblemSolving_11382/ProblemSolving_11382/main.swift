362a11573f1f500b72e8bec5d924da9c73302502
Lee Youjin <kynhun20@gachon.ac.kr>
2023-04-18 02:42:16 +0900

    baekjoon 11382 update

---
+++ ProblemSolving_11382/ProblemSolving_11382/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProblemSolving_11382
+//
+//  Created by YOUJIM on 2023/04/18.
+//
+
+import Foundation
+
+let line = readLine()!.components(separatedBy: " ").map {Int($0)!}
+
+print(line[0]+line[1]+line[2])
