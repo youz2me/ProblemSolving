fa840253a8b86f0657a1beaf991dc0f6c793a3fc
Lee Youjin <kynhun20@gachon.ac.kr>
2023-04-27 14:06:15 +0900

    Baekjoon 2745 update

---
+++ ProblemSolving_2745/ProblemSolving_2745/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  ProblemSolving_2745
+//
+//  Created by YOUJIM on 2023/04/27.
+//
+
+import Foundation
+
+let input = readLine()!.components(separatedBy: " ")
+
+var result : Int = Int(input[0], radix: Int(input[1])!)!
+
+print(result)
