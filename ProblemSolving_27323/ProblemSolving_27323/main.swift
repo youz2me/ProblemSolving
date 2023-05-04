cbf472c70f43003755b22c0222a93af387de3a09
Lee Youjin <kynhun20@gachon.ac.kr>
2023-05-04 15:18:23 +0900

    baekjoon 27323 update

---
+++ ProblemSolving_27323/ProblemSolving_27323/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  ProblemSolving_27323
+//
+//  Created by YOUJIM on 2023/05/04.
+//
+
+import Foundation
+
+let num1 = Int(readLine()!)!
+let num2 = Int(readLine()!)!
+print( num1 * num2 )
+
