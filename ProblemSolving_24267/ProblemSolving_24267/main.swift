2221425b365fd515311eeadd96478d905eb40e6b
youz2me <kynhun20@gachon.ac.kr>
2023-05-15 16:35:44 +0900

    baekjoon 24267 update

---
+++ ProblemSolving_24267/ProblemSolving_24267/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProblemSolving_24267
+//
+//  Created by YOUJIM on 2023/05/15.
+//
+
+import Foundation
+
+let num = Int(readLine()!)!
+print(num * (num - 1) * (num - 2) / 6)
+print("3")
