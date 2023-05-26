5267ef61c0d0f80e5c2e248d36c2ffa3b2318c5c
youz2me <kynhun20@gachon.ac.kr>
2023-05-26 16:58:24 +0900

    beakjoon 1427 update

---
+++ ProblemSolving_1427/ProblemSolving_1427/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  ProblemSolving_1427
+//
+//  Created by YOUJIM on 2023/05/26.
+//
+//
+//import Foundation
+
+var input = Array(readLine()!).sorted().reversed()
+for num in input { print(num, terminator: "") }
