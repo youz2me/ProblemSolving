15340347ad63b55419d99f47edbf611ac2f32d37
youz2me <kynhun20@gachon.ac.kr>
2023-05-30 17:25:45 +0900

    baekjoon 18870 update

---
+++ ProblemSolving_18870/ProblemSolving_18870/main.swift
@@ -0,0 +1,75 @@
+//
+//  main.swift
+//  ProblemSolving_18870
+//
+//  Created by YOUJIM on 2023/05/30.
+//
+
+import Foundation
+
+final class FileIO {
+    private let buffer: Data
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        self.buffer = try! fileHandle.readToEnd()!
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer {
+            index += 1
+        }
+        guard index < buffer.count else { return 0 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+
+
+    @inline(__always) func readString() -> String {
+            var str = ""
+            var now = read()
+
+            while now == 10
+                    || now == 32 { now = read() }
+
+            while now != 10
+                    && now != 32 && now != 0 {
+                str += String(bytes: [now], encoding: .ascii)!
+                now = read()
+            }
+
+            return str
+        }
+  }
+
+var file = FileIO()
+
+var numOfInput = file.readInt()
+
+var inputs = [Int]()
+for _ in 0 ..< numOfInput { inputs.append(file.readInt())}
+var inputsSet = Array(Set(inputs).sorted())
+
+var result = ""
+
+for input in inputs {
+    result.write("\(inputsSet.firstIndex(of: input)!) ")
+}
+
+print(result)
