8027a110adeb7369a66e13da4dc5cebbca192537
youz2me <kynhun20@gachon.ac.kr>
2023-06-03 15:18:42 +0900

    beakjoon 5347 update

---
+++ ProblemSolving_5347/ProblemSolving_5347/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  ProblemSolving_5347
+//
+//  Created by YOUJIM on 2023/06/03.
+//
+
+for _ in 0 ..< Int(readLine()!)! {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    var num1 = 0
+    var num2 = max(input[0], input[1])
+    var num3 = min(input[0], input[1])
+    
+    while num3 != 0 {
+        num1 = num2
+        num2 = num3
+        num3 = num1 % num2
+    }
+    print(input[0] * input[1] / num2)
+}
+
