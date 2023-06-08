fbcdd855bebd9503bcd67b72824b0bf484911556
youz2me <kynhun20@gachon.ac.kr>
2023-06-08 21:57:16 +0900

    baekjoon 1259 update

---
+++ ProblemSolving_1259/ProblemSolving_1259/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  ProblemSolving_1259
+//
+//  Created by YOUJIM on 2023/06/08.
+//
+
+//import Foundation
+
+while true {
+    let nums = Array(readLine()!)
+    if nums[0] == "0" { break }
+    if nums.reversed() == nums { print("yes")}
+    else { print("no")}
+}
+
