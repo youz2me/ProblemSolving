c8f0cd198390216a5af34ec70f60bac91c86345d
youz2me <kynhun20@gachon.ac.kr>
2023-06-28 23:23:00 +0900

    baekjoon 6840 update

---
+++ ProblemSolving_1966/ProblemSolving_1966/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  ProblemSolving_1966
+//
+//  Created by YOUJIM on 2023/06/28.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ ProblemSolving_6840/ProblemSolving_6840/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  ProblemSolving_6840
+//
+//  Created by YOUJIM on 2023/06/28.
+//
+
+//import Foundation
+
+var dishes = [Int]()
+
+dishes.append(Int(readLine()!)!)
+dishes.append(Int(readLine()!)!)
+dishes.append(Int(readLine()!)!)
+
+print(dishes.sorted()[1])
