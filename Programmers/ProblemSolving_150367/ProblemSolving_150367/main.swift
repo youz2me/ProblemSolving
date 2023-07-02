a836d48a74814f4af96312b1612f61d0705206ee
youz2me <kynhun20@gachon.ac.kr>
2023-07-02 12:14:45 +0900

    baekjoon 11047 update

--- Programmers/ProblemSolving_150367/ProblemSolving_150367/main.swift
+++ Programmers/ProblemSolving_150367/ProblemSolving_150367/main.swift
@@ -7,5 +7,19 @@
 
 import Foundation
 
-print("Hello, World!")
+func checkBinaryTree(_ start: Int, _ end: Int, _ decimal: String) -> Int {
+    return 1
+}
 
+func solution(_ numbers:[Int64]) -> [Int] {
+    var result = [Int]()
+    
+    for number in numbers {
+        var decimal = String(number, radix: 2)
+        result.append(checkBinaryTree(0, (decimal.count - 1), decimal))
+    }
+    
+    return result
+}
+
+print(solution([7, 42, 5]))
