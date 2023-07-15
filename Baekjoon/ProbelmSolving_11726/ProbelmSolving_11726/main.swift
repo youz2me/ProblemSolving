30c505ba059e3fc0fd0665693997ea46a985d218
youz2me <kynhun20@gachon.ac.kr>
2023-07-15 14:33:48 +0900

    baekjoon 11726 2트

--- Baekjoon/ProbelmSolving_11726/ProbelmSolving_11726/main.swift
+++ Baekjoon/ProbelmSolving_11726/ProbelmSolving_11726/main.swift
@@ -10,9 +10,15 @@ import Foundation
 var n = Int(readLine()!)!
 var memo = [Int](repeating: -1, count: 1001)
 
-memo[1] = 1
-memo[2] = 2
 
-for i in 3...n { memo[i] = (memo[i - 2] + memo[i - 1]) % 10007 }
+if n == 1 { print(1) }
+else if n == 2 { print(2) }
+else {
+    memo[1] = 1
+    memo[2] = 2
+    
+    for i in 3...n { memo[i] = ((memo[i - 2] % 10007) + (memo[i - 1] % 10007)) % 10007 }
 
-print(memo[n])
+    print(memo[n])
+}
+
