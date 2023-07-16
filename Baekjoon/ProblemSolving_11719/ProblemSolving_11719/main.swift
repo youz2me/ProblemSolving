400d9c9fef5ef7915f188197d0d0308afcc26951
youz2me <kynhun20@gachon.ac.kr>
2023-07-16 22:22:34 +0900

    baekjoon 11719 update

---
+++ Baekjoon/ProblemSolving_11719/ProblemSolving_11719/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  ProblemSolving_11719
+//
+//  Created by YOUJIM on 2023/07/16.
+//
+
+import Foundation
+
+var lines = [String]()
+
+while let line = readLine() { print(line) }
+
