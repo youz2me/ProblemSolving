e9f9455284a7e6baa58cb2014715ac8e1072109a
youz2me <kynhun20@gachon.ac.kr>
2023-08-09 23:39:04 +0900

    Baekjoon 26711

---
+++ Baekjoon/ProblemSolving_26711/ProblemSolving_26711/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  ProblemSolving_26711
+//
+//  Created by YOUJIM on 2023/08/09.
+//
+
+import Foundation
+
+let a = Int(readLine()!)!
+let b = Int(readLine()!)!
+
+print(a + b)
+
