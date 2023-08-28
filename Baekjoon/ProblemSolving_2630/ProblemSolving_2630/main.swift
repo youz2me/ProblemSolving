16e4671667d0732774ab41ff91dee447a10aaaf9
youz2me <kynhun20@gachon.ac.kr>
2023-08-28 23:45:06 +0900

    baekjoon 2630 update

---
+++ Baekjoon/ProblemSolving_2630/ProblemSolving_2630/main.swift
@@ -0,0 +1,42 @@
+//
+//  main.swift
+//  ProblemSolving_2630
+//
+//  Created by YOUJIM on 2023/08/28.
+//
+
+import Foundation
+
+var board = [[Int]]()
+var count = [0, 0]
+
+for _ in 0 ..< Int(readLine()!)! {
+    board.append(readLine()!.split(separator: " ").map { Int(String($0))! })
+}
+
+func check(_ x: Int, _ y: Int, _ width: Int) -> Bool {
+    for i in x ..< x + width {
+        for j in y ..< y + width {
+            if board[x][y] != board[i][j] { return false }
+        }
+    }
+    
+    return true
+}
+
+func confetti(_ x: Int, _ y: Int, _ width: Int) {
+    if check(x, y, width) {
+        count[board[x][y]] += 1
+        return
+    }
+    
+    let w = width / 2
+    
+    for i in 0 ..< 2 {
+        for j in 0 ..< 2 { confetti(x + i * w, y + j * w, w) }
+    }
+}
+
+confetti(0, 0, board.count)
+
+for c in count { print(c) }
