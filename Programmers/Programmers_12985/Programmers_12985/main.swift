79d94b7611ff668b481172a856dec2e2ef8a84fb
youz2me <kynhun20@gachon.ac.kr>
2024-11-07 00:34:48 +0900

    프로그래머스 예상 대진표

--- Programmers/Programmers_12985/Programmers_12985/main.swift
+++ Programmers/Programmers_12985/Programmers_12985/main.swift
@@ -7,24 +7,21 @@
 
 import Foundation
 
-func solution(_ n:Int, _ a:Int, _ b:Int) -> Any
-{
-    var answer = 1
+func solution(_ n:Int, _ a:Int, _ b:Int) -> Int {
+    var answer = 0
     var A = a
     var B = b
     
-    while A != B {
+    while true {
+        if A == B {
+            break
+        }
         A = (A + 1) / 2
         B = (B + 1) / 2
-        
         answer += 1
-        
-        if a == b {
-            answer -= 1
-        }
     }
     
-    return answer - 1
+    return answer
 }
 
 print(solution(8, 4, 7))
