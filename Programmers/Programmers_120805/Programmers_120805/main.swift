dabc07ed59614409d710e21b35c293b0412efd1e
youz2me <kynhun20@gachon.ac.kr>
2024-11-09 03:30:10 +0900

    프로그래머스 몫 구하기

---
+++ Programmers/Programmers_120805/Programmers_120805/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Programmers_120805
+//
+//  Created by YOUJIM on 9/11/24.
+//
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 / num2
+}
