5acf2195b98eb5a6a8810bf5e271f37c0840ad71
youz2me <kynhun20@gachon.ac.kr>
2024-11-08 22:35:10 +0900

    프로그래머스 나머지 구하기

---
+++ Programmers/Programmers_120810/Programmers_120810/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Programmers_120810
+//
+//  Created by YOUJIM on 11/8/24.
+//
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 % num2
+}
