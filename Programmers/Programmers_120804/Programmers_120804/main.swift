2644f4e080dce78d1982a545ac2c76df6e71e8c7
youz2me <kynhun20@gachon.ac.kr>
2024-11-11 23:34:08 +0900

    프로그래머스 두 수의 곱

---
+++ Programmers/Programmers_120804/Programmers_120804/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Programmers_120804
+//
+//  Created by YOUJIM on 11/11/24.
+//
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 * num2
+}
+
